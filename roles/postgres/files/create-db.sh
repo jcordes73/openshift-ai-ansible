b42a11218757e73de605f30c9edda4afe4849398
Jochen Cordes <jcordes@jcordes-thinkpadt14gen5.rmtde.csb>
2026-09-24 15:10:55 +0200

    Fixed Postgres database creation issue (fails if database already exists.

--- roles/postgres/files/create-db.sh
+++ roles/postgres/files/create-db.sh
@@ -1,5 +1,5 @@
 #psql -d DATABASE -c "SELECT pg_terminate_backend(pg_stat_activity.pid) FROM pg_stat_activity WHERE datname = current_database() AND pg_stat_activity.pid <> pg_backend_pid();"
 #dropdb DATABASE
-createdb DATABASE
+createdb DATABASE || true
 psql -c "alter user postgres with encrypted password 'postgres';"
 psql -c "grant all privileges on database DATABASE to postgres;"
